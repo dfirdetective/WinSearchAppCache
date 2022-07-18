f1d67d2d73a429e2257cb1859da34a8d5dc0aed3
DFIRDetective | Cassie <88520889+dfirdetective@users.noreply.github.com>
2022-07-17 20:48:57 -0500

    Update AppCacheParse.py

--- AppCacheParse.py
+++ AppCacheParse.py
@@ -1,12 +1,16 @@
-'''This will not work right now as I haven't added navigation to the right folder. 
-I was just running tests to view the data in different samples from the same folder
-with a known name.
+'''This will not work intuitively right now as I haven't added 
+automated collection and parsing. 
+
+To get this to work, you will have to collect your own AppCache
+file and rename to AppCache.txt or change the 
+filename = "AppCache.txt" to your filename. 
 '''
 
 import json
 import csv
 
 filename = "AppCache.txt"
+# TODO Automate artifact collection and parsing.
 
 with open(filename) as appCache:
     data = json.load(appCache)
@@ -24,7 +28,6 @@ with open(filename) as appCache:
                     writehead.append(y)
             csv_writer.writerow(writehead)
             count += 1
-#        csv_writer.writerow(d.values())
         dValues = list(d.values())
         dData = []
         for item in dValues:
