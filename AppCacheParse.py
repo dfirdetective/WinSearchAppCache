d3b1758b7c74227433d093563072b1f47b6bdf3c
DFIRDetective | Cassie <88520889+dfirdetective@users.noreply.github.com>
2022-07-19 11:48:41 -0500

    Update AppCacheParse.py
    Added notes at the top. Changed file/folder structure and names so I could automate running instances over the course of the day and contain them all in one folder.

--- AppCacheParse.py
+++ AppCacheParse.py
@@ -1,6 +1,12 @@
 """
-This is a v2 test.
-Intent: Parse app data from within AppCacheXXXXXXXXXXXXXXXXXX.txt
+AppCacheParse.py
+Author: Cassie Doemel
+Intent: AppCacheXXXXXXXXXXXXXXXXXX.txt to csv for readability for my research
+Current Iteration: Collects and parses artifact from host Windows machine. 
+Run from your desired output directory. Will create WinSearchAppCache folder 
+with subdirectory of the POSIX timestamp at run time. Directory will contain 
+raw AppCache file, SettingsCache file, and the cacheOutput.csv parsed data.
+Future: Create CLI args to allow for parse-only or mounted directory selection.
 """
 
 from pathlib import Path
@@ -8,22 +14,27 @@ import json
 import csv
 import os
 import shutil
+from datetime import datetime
+
 
 homeDir = Path.home()
 p = str(homeDir)
+dt = datetime.now()
+ts = datetime.timestamp(dt)
 appCacheLoc = p + '\\AppData\\Local\\Packages\\Microsoft.Windows.Search_cw5n1h2txyewy\\LocalState\\DeviceSearchCache'
-outputPath = Path.cwd() / 'WinSearchAppCache'
+outputPath = Path.cwd() / 'WinSearchAppCache' / str(ts)
 
+#   TODO create command-line arguments to run against whatever is needed
 
 def file_exists(file):
     with open(file) as appCache:
         data = json.load(appCache)
         outputFile = str(outputPath) + "\\cacheOutput.csv"
-        cacheOutput = open(outputFile, 'w')
+        cacheOutput = open(outputFile, 'w')     # Creates the output file
         csv_writer = csv.writer(cacheOutput)
         count = 0
         for d in data:
-            if count == 0:
+            if count == 0:                      # Writes the 0 row as headers
                 header = list(d.keys())
                 writehead = []
                 for x in header:
@@ -34,19 +45,19 @@ def file_exists(file):
                 count += 1
             dValues = list(d.values())
             dData = []
-            for item in dValues:
+            for item in dValues:                # Writes the rest as values
                 vKeys = list(item.keys())
                 for v in vKeys:
-                    if v.startswith("Value"):
+                    if v.startswith("Value"):   # Strips off "Type: "
                         dData.append(item[v])
             csv_writer.writerow(dData)
     print("Parsed data printed to " + outputFile)
-    appCache.close()
+    appCache.close()                            # Closes the file so it can be viewed.
 
 
 def find_app_cache(username):
     try:
-        shutil.copytree(appCacheLoc, outputPath)
+        shutil.copytree(appCacheLoc, outputPath)    # Copies AppCache & SettingsCache
         print("Host folder copied to " + str(outputPath))
         for folderName, subfolders, filenames in os.walk(outputPath):
             for filename in filenames:
@@ -54,17 +65,17 @@ def find_app_cache(username):
                     cachefile = Path(outputPath) / filename
                     file_exists(cachefile)
 
-    except:
-        print("Error. File or folder not found.")
-
+    except Exception as inst:
+        print(type(inst), " - ", inst.args)
 
 def target_host():
-    if str(homeDir.anchor)[0].isalpha():
-        username = homeDir.parts[2]
+    if str(homeDir.anchor)[0].isalpha():    # checking for C: at root
+        username = homeDir.parts[2]         # pulling the username - was needed in previous iterations TODO remove
         find_app_cache(username)
     else:
         print("This is intended to target the host machine by locating C: drive.")
 
-print("Running WinSearchAppCache...")
+print("Running WinSearchAppCache...     Start time: ", dt)
 target_host()
-print("WinSearchAppCache Complete!")
+dt = datetime.now()
+print("WinSearchAppCache Complete!     End time: ", dt)
