f4f90003c54852f275bb809fff1ccfdd5fba3b28
DFIRDetective | Cassie <88520889+dfirdetective@users.noreply.github.com>
2022-07-27 19:56:14 -0500

    Update AppCacheParse.py
    Attempts to parse SettingsCache, however, a Defender row consistently throws a UnicodeError that I'm trying to work around.
    
    Still only targeting my machine for research/practice.

--- AppCacheParse.py
+++ AppCacheParse.py
@@ -1,12 +1,13 @@
 """
-AppCacheParse.py
+AppCacheParse.py v3
 Author: Cassie Doemel
 Intent: AppCacheXXXXXXXXXXXXXXXXXX.txt to csv for readability for my research
-Current Iteration: Collects and parses artifact from host Windows machine. 
-Run from your desired output directory. Will create WinSearchAppCache folder 
-with subdirectory of the POSIX timestamp at run time. Directory will contain 
-raw AppCache file, SettingsCache file, and the cacheOutput.csv parsed data.
-Future: Create CLI args to allow for parse-only or mounted directory selection.
+Current Iteration: Collects and parses artifact from host Windows machine.
+Run from your desired output directory. Will create WinSearchAppCache folder
+with subdirectory of the POSIX timestamp at run time. Directory will contain
+raw AppCache file, SettingsCache file, and the two Output.csv parsed data files.
+
+Future TODO: Create CLI args to allow for parse-only or mounted directory selection.
 """
 
 from pathlib import Path
@@ -16,7 +17,6 @@ import os
 import shutil
 from datetime import datetime
 
-
 homeDir = Path.home()
 p = str(homeDir)
 dt = datetime.now()
@@ -24,56 +24,89 @@ ts = datetime.timestamp(dt)
 appCacheLoc = p + '\\AppData\\Local\\Packages\\Microsoft.Windows.Search_cw5n1h2txyewy\\LocalState\\DeviceSearchCache'
 outputPath = Path.cwd() / 'WinSearchAppCache' / str(ts)
 
-#   TODO create command-line arguments to run against whatever is needed
 
-def file_exists(file):
-    with open(file) as appCache:
+#   TODO create command-line arguments to run against whatever you want.
+
+def acfile_exists(file):
+    with open(file, encoding='utf=8') as appCache:
         data = json.load(appCache)
-        outputFile = str(outputPath) + "\\cacheOutput.csv"
-        cacheOutput = open(outputFile, 'w')     # Creates the output file
+        outputFile = str(outputPath) + "\\" + file.stem + "-Output.csv"
+        cacheOutput = open(outputFile, 'w')  # Creates the output file
         csv_writer = csv.writer(cacheOutput)
         count = 0
         for d in data:
-            if count == 0:                      # Writes the 0 row as headers
+            if count == 0:  # Writes the 0 row as headers
                 header = list(d.keys())
-                writehead = []
+                acwritehead = []
                 for x in header:
                     if x[0:7] == "System.":
                         y = x[7:]
-                        writehead.append(y)
-                csv_writer.writerow(writehead)
+                        acwritehead.append(y)
+                csv_writer.writerow(acwritehead)
                 count += 1
             dValues = list(d.values())
             dData = []
-            for item in dValues:                # Writes the rest as values
-                vKeys = list(item.keys())
-                for v in vKeys:
-                    if v.startswith("Value"):   # Strips off "Type: "
+            for item in dValues:  # Writes the rest as values
+                for v in item.keys():
+                    if v.startswith("Value"):  # Strips off "Type: "
+                        # if v.islist, iterate over list in some way to clean it up?
+                        # .strip(\r\n
                         dData.append(item[v])
             csv_writer.writerow(dData)
     print("Parsed data printed to " + outputFile)
-    appCache.close()                            # Closes the file so it can be viewed.
+    appCache.close()  # Closes the file so it can be viewed.
 
 
-def find_app_cache(username):
+def scfile_exists(file):
+    with open(file, encoding='utf=8') as scCache:
+        data = json.load(scCache)
+        outputFile = str(outputPath) + "\\" + file.stem + "-Output.csv"
+        cacheOutput = open(outputFile, 'w')  # Creates the output file
+        csv_writer = csv.writer(cacheOutput)
+        count = 0
+        try:
+            for d in data:
+                if count == 0:  # Writes the 0 row as headers
+                    scwritehead = ['ParsingName', 'ActivationContext', 'SmallLogoPath', 'PageID', 'SettingID',
+                                   'HostID', 'Condition', 'Comment', 'HighKeywords']
+                    csv_writer.writerow(scwritehead)
+                    count += 1
+                dValues = list(d.values())
+                dData = []
+                for item in dValues:  # Writes the rest as values
+                    for v in item.keys():
+                        if v.startswith("Value"):  # Strips off "Type: "
+                            dData.append(item[v])
+                csv_writer.writerow(dData)
+
+        except UnicodeError:
+            print("Error: File closed incomplete due to Unicode error.")    # Repeated fails on Defender line
+    print("Parsed data printed to " + outputFile)
+    scCache.close()  # Closes the file so it can be viewed.
+
+
+def find_app_cache():
     try:
-        shutil.copytree(appCacheLoc, outputPath)    # Copies AppCache & SettingsCache
+        shutil.copytree(appCacheLoc, outputPath)  # Copies AppCache & SettingsCache
         print("Host folder copied to " + str(outputPath))
         for folderName, subfolders, filenames in os.walk(outputPath):
             for filename in filenames:
-                if "AppCache" in str(filename):
-                    cachefile = Path(outputPath) / filename
-                    file_exists(cachefile)
+                cachefile = Path(outputPath) / filename
+                if "AppCache" in filename:
+                    acfile_exists(cachefile)
+                else:
+                    scfile_exists(cachefile)
 
     except Exception as inst:
         print(type(inst), " - ", inst.args)
 
+
 def target_host():
-    if str(homeDir.anchor)[0].isalpha():    # checking for C: at root
-        username = homeDir.parts[2]         # pulling the username - was needed in previous iterations TODO remove
-        find_app_cache(username)
+    if str(homeDir.anchor)[0].isalpha():  # checking for root drive letter to ID as Windows
+        find_app_cache()
     else:
-        print("This is intended to target the host machine by locating C: drive.")
+        print("This is intended to target the host Windows machine by locating root drive at this time.")
+
 
 print("Running WinSearchAppCache...     Start time: ", dt)
 target_host()
